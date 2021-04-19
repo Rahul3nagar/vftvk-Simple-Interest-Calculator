385805611592d709e77ee7824085649a063282fe
Rahul <rahul3nagar@gmail.com>
2021-04-19 12:10:51 +0000

    save changes

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
     
+    var principal = document.getElementById("principal").value;
+    // check if princiap value is negative or zero
+    if(principal <=0){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return false;
+    }
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+// display result msg
+    var resultMsg = 'If you deposite '+principal+", <br>at an interest rate of "+
+    rate+"%. <br>You will receive an amount of "+interest+",<br>in the year "+year;
+    document.getElementById("result").innerHTML = resultMsg;
+}
+// function to update changed value by range
+function updateRate(){
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerHTML = rateval;
 }
         
